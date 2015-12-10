a3779dff4944f470fba3d925e05e6febc33006c1
App Academy Student <student@appacademy.io>
2015-12-10 11:35:21 -0800

    first commit

---
+++ frontend/pokedex.jsx
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+// console.log("i'm running!!!!!!");
+
+document.addEventListener("DOMContentLoaded", function() {
+  ReactDOM.render(<div>Hi</div>, document.getElementById('root'));
+  }
+);
